689284d7f6f46644a0c4d4ccff088200b106e517
TurkFlame <luizfelipemarcondesdelima@gmail.com>
2023-04-11 14:30:13 -0300

    initial commit

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,20 @@ const routes = express.Router()
 const crypto = require('crypto');
 const sha256 = require('crypto-js/sha256');
 
-let string_in_sha256 = ""
-
 //Inserir dados
-routes.post('/:string',(req, res)=>{
-    string_in_sha256 = sha256(req.params.string)
-    return res.json({ hash: string_in_sha256.toString() })
+
+routes.post('/', (req, res) => {
+    const { data, access_key, secret_key } = req.body
+
+    const dataHash = sha256(data)
+    const accessKeyHash = sha256(access_key)
+    const secretKeyHash = sha256(secret_key)
+
+    return res.json({
+        data: dataHash.toString(),
+        access_key: accessKeyHash.toString(),
+        secret_key: secretKeyHash.toString()
+    })
 })
 
 module.exports = routes
